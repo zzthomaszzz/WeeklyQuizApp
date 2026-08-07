b7a8d1e425e08c369d4f09b28e20f69e06c0e099
Thomas Minh Nguyen <thomasnguyen8503@gmail.com>
2026-08-07 21:06:46 +1200

    Login page now display a form user and password with a submit button

---
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,18 @@
+
+export function LoginPage() {
+    return (
+        <>
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" name="password" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </>
+    );
+}
